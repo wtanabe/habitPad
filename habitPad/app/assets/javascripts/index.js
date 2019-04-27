1b9f49f7f652ac62a9c169b3f5ed3bcbecac4c74
wtanabe <tibakenmatudosi@gmail.com>
2019-04-27 22:53:44 +0900

    add create_item and create_repeat_switch

--- habitPad/app/assets/javascripts/index.js
+++ habitPad/app/assets/javascripts/index.js
@@ -5,15 +5,34 @@ window.onload = function() {
 
 
  
+
+
+	
+
+	var add_repeat_switch = document.getElementById('add_repeat_switch');
+	add_item.addEventListener("click",create_item);
+	add_repeat_switch.addEventListener("click",create_repeat_switch);
+
+
 }
 
+// 繰り返す対象の項目を生成
+function create_item(){
+		var item = document.getElementById('item');
+		html_content = 
+		'<div id="repeat_switch_top">' +
+		'<input type="text" name="">' +
+		'<input type="button" name="" id="add_repeat_switch" onchange="create_repeat_switch">' +
+		'</div>' +
+		'<div id="repeat_switch_bottom">' +
+		'<input type="checkbox" name="" id="checkbox">' +
+		'<input type="text" name="">' +
+		'</div>';
+		item.insertAdjacentHTML('beforeend',html_content);
+}
+
+// 繰り返しボタンを生成
 function create_repeat_switch(){
-	var repeat_switch = document.getElementById('repeat_switch');
-
-	repeat_switch.insertAdjacentHTML('beforebegin','<div id="repeat_switch"><input type="checkbox" name="" id="checkbox"><input type="text" name=""></div>');
-
+		var repeat_switch = document.getElementById('repeat_switch');
+		repeat_switch.insertAdjacentHTML('beforeend','<div class="repeat_switch_copy"><input type="checkbox" name="" id="checkbox"><input type="text" name=""></div>');
 }
-
-var add_repeat_switch = document.getElementById('add_repeat_switch');
-console.log(add_repeat_switch);
-add_repeat_switch.addEventListener("click",create_repeat_switch);
